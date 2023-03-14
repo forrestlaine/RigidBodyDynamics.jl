dbadcb9fc05bdd06c2e5a799aceef874aaac69c1
Forrest Laine <forrest@Mac-Studio.local>
2023-03-14 17:18:38 -0500

    typo

--- src/mechanism_algorithms.jl
+++ src/mechanism_algorithms.jl
@@ -697,9 +697,9 @@ function contact_dynamics!(result::DynamicsResult{T, M}, state::MechanismState{X
                 @inbounds c = points[i]
                 point = body_to_root * location(c)
                 if isnothing(frictionless_direction(c))
-                    frictionless_direction = nothing
+                    fl_direction = nothing
                 else
-                    friectionless_direction = body_to_root * frictionless_direction(c)
+                    fl_direction = body_to_root * frictionless_direction(c)
                 end
                 velocity = point_velocity(twist, point)
                 states_for_point = states_for_body[i]
@@ -714,7 +714,7 @@ function contact_dynamics!(result::DynamicsResult{T, M}, state::MechanismState{X
                     if point_inside(primitive, point)
                         separation, normal = Contact.detect_contact(primitive, point)
                         force = Contact.contact_dynamics!(contact_state_deriv, contact_state,
-                            model, -separation, velocity, normal, frictionless_direction)
+                            model, -separation, velocity, normal, fl_direction)
                         wrench += Wrench(point, force)
                     else
                         Contact.reset!(contact_state)
